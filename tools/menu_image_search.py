bfec7127c4e0a7965293d9a3cdc83b4278ab62f3
Brian Macdonald <brianmacdonald@users.noreply.github.com>
2023-10-14 14:56:06 -0600

    Update menu_image_search.py

--- tools/menu_image_search.py
+++ tools/menu_image_search.py
@@ -7,12 +7,7 @@ import easyocr
 from PIL import Image
 from langchain.chat_models import ChatOpenAI
 
-def image_menu_search(image_url):
-    if not os.path.exists("images"):
-        os.mkdir("images")
-    img_path = "images/image.jpg"
-    with open(img_path, 'wb') as f:
-        f.write(requests.get(image_url).content)
+def image_menu_search(img_path: str):
     image_data = extract_image_data(img_path)
     return image_data
 
