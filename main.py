898198f80841f67c709ebdc93fb187f7a128daf9
kaylynnwatson <kaylynnwatson@focusedlabs.io>
2023-10-14 15:35:24 -0600

    Revert "Merge pull request #1 from brianmacdonald/ocr-menu"
    This reverts commit a4990f24dca06dbc8ef9945ddb4dfffb31d98e2d, reversing
    changes made to 9b5bb4f626362fe4b4ae8379b48b2966749f528c.

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import logging
 import sys
 from contextlib import asynccontextmanager
-import random
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from models.question import Question
 from query_service import QueryService
-from query_image_service import QueryImageService
 
 load_dotenv()
 
@@ -19,7 +17,6 @@ allowed_origins = [
 ]
 
 query_service = QueryService()
-query_image_service = QueryImageService()
 
 
 def init_logging():
@@ -53,13 +50,6 @@ async def root():
 @app.post("/query/")
 async def query(question: Question):
     return query_service.query(question=question)
-
-
-@app.post("/image/")
-async def image(file: Annotated[bytes, UploadFile()], question: Question):
-    filename = random.getrandbits(128) + "-" + file.filename
-    open(filename, "wb").write(file)
-    return query_image_service.image_menu_search(image_path=filename, question=question)
 
 
 if __name__ == "__main__":
--- query_service.py
+++ query_service.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from menu_parse_agent import MenuParseAgent
 from models.question import Question
 from restaurant_retrieval_agent import RestaurantRetrievalAgent
-from image_menu_search import image_menu_search
 
 load_dotenv()
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
@@ -48,23 +47,6 @@ def create_menu_parse_prompt(restaurant_details: str, dietary_restrictions: str)
     Your dietary restrictions are {dietary_restrictions}
     """
 
-def create_menu_image_parse_prompt(restaurant_details: str, dietary_restrictions: str):
-    return f"""
-    # Goal
-    You are trying given a restaurant menu, you are trying to find out if the menu is compatible with your dietary restrictions.
-
-    # This is the process you will follow to find this information.
-    For each restaurant in the list:
-    1. You will then learn what the menu says.
-    2. Based off of the menu content, is this menu compatible with your dietary restrictions?
-    Answer as succinctly as possible, but make sure to include the restaurant name.
-
-    # Restaurant Info
-    The restaurant menu included in the following: {restaurant_details}
-
-    # Your dietary restrictions
-    Your dietary restrictions are {dietary_restrictions}
-    """
 
 class QueryService:
 
@@ -84,31 +66,6 @@ class QueryService:
                 try:
                     answer += self.menu_parse_agent.run_agent(
                         create_menu_parse_prompt(restaurant_string, question.dietaryRestrictions))
-                except Exception as e:
-                    print(f"ERROR --- address:{question.address} --- {e} ")
-        except Exception as e:
-            print(f"ERROR --- address:{question.address} --- {e} ")
-            raise e
-        return answer
-
-
-class ImageQueryService:
-
-    def __init__(self):
-        self.restaurant_retrieval_agent = RestaurantRetrievalAgent()
-        self.menu_parse_agent = MenuParseAgent()
-
-    def query(self, image_path, question: Question):
-        try:
-            restaurant_info = image_menu_search(image_data=image_path)
-            restaurants = restaurant_info.strip().split('\n')
-            answer = ''
-
-            for restaurant in restaurants:
-                restaurant_string = f"{restaurant}"
-                try:
-                    answer += self.menu_parse_agent.run_agent(
-                        create_menu_image_parse_prompt(restaurant_string, question.dietaryRestrictions))
                 except Exception as e:
                     print(f"ERROR --- address:{question.address} --- {e} ")
         except Exception as e:
